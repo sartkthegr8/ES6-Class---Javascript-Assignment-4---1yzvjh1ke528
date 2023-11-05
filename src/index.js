5b8b35ce4357bd94ab445008aa8946c18afa57ac
sartkthegr8 <49450634+sartkthegr8@users.noreply.github.com>
2023-11-05 11:29:20 +0000

    #

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import {createRoot} from "react-dom/client"
-import App from "./components/App"
+class API{
+  #secure=true;
+  constructor(url, method='GET')
+  {
+      this.url=url
+      this.method=method
+      this.#secure=url.startsWith('https')
+  }
+  isSecure(){
+      return this.#secure
+  }
+  updateUrl(newUrl) {
+      this.url = newUrl;
+      this.#secure = newUrl.startsWith('https');
+  }
 
-createRoot(
-  document.getElementById("root")
-).render(<App />)
+}
+
+// const s = new API('http://api.com/api/hello');
+// console.log(s.isSecure()); // false
+
+// s.updateUrl('https://api.com/api/hello');
+// console.log(s.isSecure()); // true
+
+// console.log(s.url); // https://api.com/api/hello
+// console.log(s.method); // GET
+// console.log(s.secure); // undefined (because it's a private field)
+
+
+
+//Do not remove this code
+module.exports = { API }
